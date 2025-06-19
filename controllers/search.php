5dd78613076c9e4c87b02d1c6525a61baedbf495
godfrey <edehatser@gmail.com>
2025-06-19 09:46:14 +0100

    .

--- controllers/search.php
+++
@@ -1,8 +0,0 @@
-<?php
-require '../connection.php';
-
-$search = $_GET['query'] ?? '';
-$stmt = $pdo->prepare("SELECT * FROM users WHERE name LIKE ? OR email LIKE ?");
-$stmt->execute(["%$search%", "%$search%"]);
-$results = $stmt->fetchAll();
-echo json_encode($results);
