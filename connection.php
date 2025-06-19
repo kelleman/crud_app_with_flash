c1896524e09a9bc665b23ecc6074ee67bd079970
godfrey <edehatser@gmail.com>
2025-06-19 09:32:43 +0100

    connection created

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+$host = 'localhost';
+$db = 'test';
+$user = 'root';
+$pass = '';
+
+$pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
